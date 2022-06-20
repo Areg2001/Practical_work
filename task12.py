9649c15b3a3ff7265ba076663b7aa62430c4e9e4
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-06-20 23:27:40 +0400

    Add files via upload

---
+++ task12.py
@@ -0,0 +1,11 @@
+fut = float(input('Please, enter count of futs of your height: '))
+dyuym = float(input('Please, enter count of dyuym of your height: '))
+
+def height(dyuym):
+    res = dyuym * 2.54
+    return res
+
+if (12 * fut) == dyuym:
+    print(height(dyuym))    
+    
+
