847fd715e24abbeda08b5751242bb8acb107058f
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-06-20 20:54:36 +0400

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,7 @@
+length = float(input('Please, enter width of room: '))
+width = float(input('Please, enter length of room: '))
+
+def room_area(length, width):
+    return length * width
+
+print(f'{room_area(length, width)}Մ^2')
