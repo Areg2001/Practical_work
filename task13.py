4379c05a7c47519d19998fed608bf96c35502796
Areg2001 <107401750+Areg2001@users.noreply.github.com>
2022-06-20 23:36:35 +0400

    Add files via upload

---
+++ task13.py
@@ -0,0 +1,12 @@
+distance = int(input('Please, enter distance with fut: '))
+
+def dyuym(value):
+    return value * 12
+
+def yard(value):
+    return value / 3
+
+def miles(value):
+    return value / 5280
+
+print(dyuym(distance), yard(distance), miles(distance))            
